ec50774c22709406619d889a3aa611262c787183
whitewhale6 <whitewhale6@gmail.com>
2015-09-03 17:44:33 +0900

    Light?Camera?途中あげ

--- Assets/Scripts/BoundCamera.cs
+++ Assets/Scripts/BoundCamera.cs
@@ -7,18 +7,32 @@ public class BoundCamera : MonoBehaviour {
 	public GameObject sCam;
 	Vector3 sPosition;
 	public float smooth = 10;
+	bool isTouch = false;
 
 	void Start(){
 		player = GameObject.Find ("Player");
 	}
 
 	void Update(){
-		transform.position = Vector3.Lerp (transform.position, sCam.transform.position, Time.deltaTime);
+		CameraMove ();
 		transform.LookAt (player.transform.position);
 	}
-	
-	public void OnTriggerStay(Collider coll){
-		transform.position = Vector3.Lerp (transform.position, sCam.transform.position + sCam.transform.forward - sCam.transform.up, Time.deltaTime*smooth);
+
+	void CameraMove(){
+		if (isTouch) {
+			transform.position = Vector3.Lerp (transform.position, sCam.transform.position + sCam.transform.forward + sCam.transform.forward - sCam.transform.up, Time.deltaTime * smooth);
+		} else {
+			transform.position = Vector3.Lerp (transform.position, sCam.transform.position, Time.deltaTime);
+		}
+	}
+
+	void OnTriggerStay(Collider coll){
+		isTouch = true;
+		//transform.position = Vector3.Lerp (transform.position, sCam.transform.position + sCam.transform.forward + sCam.transform.forward - sCam.transform.up, Time.deltaTime*smooth);
 		//transform.position = transform.position + transform.up*Time.deltaTime*smooth;
+	}
+
+	void OnTriggerExit(Collider coll){
+		isTouch = false;
 	}
 }
